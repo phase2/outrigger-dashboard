2c33a5bcaaab1550a3b9be042cec9e7f67a96dd2
Adam Ross <aross@phase2technology.com>
2017-12-19 21:30:50 -0800

    Add top-navbar with useful links.

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app" class="container">
-    <div class="jumbotron">
+
+    <nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">
+      <a class="navbar-brand" href="#"><img src="../assets/logo.png" height="40" width="40" alt="Outrigger Logo" /></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item"><a class="nav-link" href="http://outrigger.sh">What is Outrigger?</a></li>
+          <li class="nav-item"><a class="nav-link" href="http://docs.outrigger.sh">Documentation</a></li>
+          <li class="nav-item"><a class="nav-link" href="http://slack.outrigger.sh/">Join us on Slack</a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="supportDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              File an Issue
+            </a>
+            <div class="dropdown-menu" aria-labelledby="supportDropdown">
+              <a class="dropdown-item" href="https://github.com/phase2/outrigger-dashboard/issues">Dashboard issues</a>
+              <a class="dropdown-item" href="https://github.com/phase2/outrigger-docs/issues">Documentation issues</a>
+              <a class="dropdown-item" href="https://github.com/phase2/rig/issues">Outrigger CLI issues</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <header class="jumbotron">
       <div class="project-title__wrapper">
-        <div class="media-left media-middle">
-          <img class="logo-image" src="../assets/logo.png">
-        </div>
         <div class="media-body">
           <h1 class="project-name">Outrigger Dashboard</h1>
         </div>
       </div>
-    </div>
-    <div class="container">
+    </header>
+    <main class="container">
       <div class="row">
         <dns-records :entries="dnsRecords"></dns-records>
       </div>
       <project-list :projects="projects"></project-list>
-    </div>
+    </main>
   </div>
 </template>
 
